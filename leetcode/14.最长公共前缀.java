235d409fa07054b08706ba4c39bec3df1cca5160
oecy <cgdstudy@qq.com>
2020-04-17 20:54:26 +0800

    4.17晚

---
+++ leetcode/14.最长公共前缀.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=java
+ *
+ * [14] 最长公共前缀
+ *
+ * https://leetcode-cn.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (36.79%)
+ * Likes:    963
+ * Dislikes: 0
+ * Total Accepted:    228.4K
+ * Total Submissions: 618.4K
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * 
+ * 示例 1:
+ * 
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 
+ * 
+ * 说明:
+ * 
+ * 所有输入只包含小写字母 a-z 。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0)//当strs为空时
+            return "";
+        if(strs.length == 1)//当字符串数组只有一个字符串时
+            return strs[0];
+        int length = strs[0].length();//
+        if(length == 0)//如果第一个字符串就为null
+            return "";
+        for(int i = 1 ; i < strs.length  ; i++){//获取短字符串长度
+            if(strs[i].length() == 0)//如果有其他字符串为null
+                return "";
+            length = Math.min( strs[i].length(), length);
+        }
+        String ans = "";
+        boolean flag = true;//是否相同标志
+        for(int i = 0 ; i < length ; i++){
+            char temp = strs[0].charAt(i);//获取第一个字符串的第i个字符
+            for(int j = 1 ; j < strs.length ; j++){//第2个字符串开始匹配
+                if( temp != strs[j].charAt(i))
+                    flag = false;
+            }
+            if(flag)//如果匹配成功
+                ans = ans + temp;
+            else{
+                if(ans.isEmpty())//全部匹配失败
+                    return "";
+                else
+                    return ans;//匹配失败但是有部分已经匹配成功
+            }
+        }
+    }
+
+    /**   官方的解法一。使用减法虽然都是暴力的方法。但是十分的简洁。
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+        while (strs[i].indexOf(prefix) != 0) {
+           prefix = prefix.substring(0, prefix.length() - 1);
+           if (prefix.isEmpty()) return "";
+        }        
+        return prefix;
+     */
+}
+// @lc code=end
+
