84af4b9032e2f608c396d94fda3a0d8b54f0359d
oecy <cgdstudy@qq.com>
2020-10-30 14:29:23 +0800

    10.30

---
+++ leetcode/463.岛屿的周长.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=463 lang=java
+ *
+ * [463] 岛屿的周长
+ */
+
+// @lc code=start
+
+/**
+ * 依次扫描每个格子，每一块岛屿都有四个边，每多连接一个岛屿就会自己少一个边
+ * 
+ * 建立一个新的二维数组存储每个节点对应的周围有多少个岛屿
+ */
+class Solution {
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    public int islandPerimeter(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) {
+                    int cnt = 0;
+                    for (int k = 0; k < 4; ++k) {
+                        int tx = i + dx[k];
+                        int ty = j + dy[k];
+                        if (tx < 0 || tx >= n || ty < 0 || ty >= m || grid[tx][ty] == 0) {
+                            cnt += 1;
+                        }
+                    }
+                    ans += cnt;
+                }
+            }
+        }
+        return ans;
+    }
+
+
+}
+// @lc code=end
+
