0eb6a6aa78f5833f3a42312cffef6f64fcb03b7c
oecy <cgdstudy@qq.com>
2020-11-10 09:41:16 +0800

    1109

---
+++ leetcode/973.最接近原点的-k-个点.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+ * @lc app=leetcode.cn id=973 lang=java
+ *
+ * [973] 最接近原点的 K 个点
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        Arrays.sort(points, new Comparator<int[]>() {
+            public int compare(int[] point1, int[] point2) {
+                return (point1[0] * point1[0] + point1[1] * point1[1]) - (point2[0] * point2[0] + point2[1] * point2[1]);
+            }
+        });
+        return Arrays.copyOfRange(points, 0, K);
+    }
+}
+// @lc code=end
+
